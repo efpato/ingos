32a73f7e514a117f5597654a9cccc425f3b7082e
Sergey Demenok <sergey.demenok@gmail.com>
2018-04-26 13:59:51 +0300

    new site

--- elements.py
+++ elements.py
@@ -1,27 +1,135 @@
 # -*- coding: utf-8 -*-
 
-from page_object import PageElement
+from time import sleep
+
+from page_object import PageElement, PageElementWrapper
+from page_object.ui import Textbox
+from selenium.common.exceptions import TimeoutException
 
 
-class ButtonElement(object):
-    def __init__(self, webelement):
-        self.el = webelement
-
-    def click(self):
-        self.el.parent.execute_script("arguments[0].click();", self.el)
+AVG = '_avg_'
 
 
-class Input(PageElement):
-    def __set__(self, instance, value):
-        if value is not None:
-            value = str(value).strip()
-            if value:
-                self.__get__(instance, instance.__class__)
-                instance.webdriver.execute_script(
-                    """$("{0}").val("{1}").change();"""
-                    .format(self._locator[1], value))
+class MultySelectTextboxWrapper(PageElementWrapper):
+    def enter_avg(self):
+        self._el.parent.execute_script(
+            """
+            $("{0}").click();
+            setTimeout(function () {{
+                $("{0}").click();
+                setTimeout(function () {{
+                    var li = $("{0}")
+                        .parent()
+                        .parent()
+                        .next()
+                        .find('li');
+                    var index = Math.floor(li.length / 2);
+                    li[index].click();
+                }}, 1000);
+            }}, 1000);
+            """.format(self._locator[1]))
+        sleep(2.1)
+
+    def enter_text(self, text):
+        self._el.parent.execute_script(
+            """
+            $("{0}").click();
+            setTimeout(function () {{
+                $("{0}").val("{1}").change();
+                setTimeout(function () {{
+                    $("{0}")
+                    .parent()
+                    .parent()
+                    .next()
+                    .find('li')
+                    .filter(function () {{
+                        return $.trim($(this).text()).toLowerCase() == "{1}";
+                    }})
+                    .click();
+                }}, 1000);
+            }}, 1000);
+            """.format(self._locator[1], text))
+        sleep(2.1)
 
 
-class Label(PageElement):
+class MultySelectTextbox(PageElement):
     def __get__(self, instance, owner):
-        return ButtonElement(super().__get__(instance, owner))
+        return MultySelectTextboxWrapper(
+            self.find(instance.webdriver), self._locator)
+
+    def __set__(self, instance, value):
+        if value is None:
+            return
+
+        value = str(value).strip().lower()
+        if not value:
+            return
+
+        try:
+            el = self.__get__(instance, instance.__class__)
+        except TimeoutException:
+            return
+
+        if value == AVG:
+            el.enter_avg()
+        else:
+            el.enter_text(value)
+
+
+class SearchTextbox(PageElement):
+    def __set__(self, instance, value):
+        if value is None:
+            return
+
+        value = str(value).strip()
+        if not value:
+            return
+
+        self.__get__(instance, instance.__class__)
+        instance.webdriver.execute_script(
+            """
+            $("{0} input").val("{1}").change();
+            setTimeout(function () {{
+                $("{0} div.search-select__field > div").click();
+                setTimeout(function () {{
+                    $("{0} div.search-select__dropdown li").first().click();
+                }}, 1000);
+            }}, 1000);
+            """.format(self._locator[1], value))
+        sleep(2.1)
+
+
+class Slider(Textbox):
+    def __set__(self, instance, value):
+        super().__set__(instance, value)
+        instance.webdriver.execute_script(
+            """
+            $("{}").blur();
+            """.format(self._locator[1]))
+
+
+class VariantsSliderWrapper(PageElementWrapper):
+    @property
+    def total(self):
+        return self._el.find_element_by_xpath(
+            ".//span[contains(@class, 'value-slider__primary-value')]").text
+
+    @property
+    def description(self):
+        return self._el.find_element_by_xpath(
+            ".//span[contains(@class, 'value-slider__secondary-value')]").text
+
+    @property
+    def variants(self):
+        return self._el.parent.execute_script(
+            """
+            return $("{}").scope().$parent.kcc.calculationResults;
+            """.format(self._locator[1]))
+#         return self._el.find_elements_by_xpath(
+#             ".//div[contains(@class, 'value-slider__slider')]/span")
+
+
+class VariantsSlider(PageElement):
+    def __get__(self, instance, owner):
+        return VariantsSliderWrapper(
+            self.find(instance.webdriver), self._locator)
--- pages.py
+++ pages.py
@@ -1,279 +1,41 @@
 # -*- coding: utf-8 -*-
 
-import logging
-from functools import wraps
-from time import sleep
+from page_object import PageObject
+from page_object.ui import Button, Link, Select
+from page_object.ui.jquery import Textbox
 
-from page_object import PageObject, PageElements
-from page_object.elements import Link, Select
-from selenium.webdriver.support.wait import WebDriverWait
-
-from elements import Label, Input
-
-
-WAIT_TIMEOUT = 60
-
-
-def safe(fn):
-    @wraps(fn)
-    def wrapped(*args, **kwargs):
-        logging.debug("==> %s (args=%s, kwargs=%s)",
-                      fn.__name__, args, kwargs)
-        try:
-            fn(*args, **kwargs)
-        except:
-            logging.warning("%s --> autoselect", fn.__name__)
-
-    return wrapped
+from elements import MultySelectTextbox, SearchTextbox, Slider, VariantsSlider
 
 
 class KaskoCalcPage(PageObject):
     URL = "https://www.ingos.ru/ru/private/auto/kasko/calc/"
+    car_fmt = "div[ng-model='vm.cxCarModel'] input[placeholder='{}']".format
+    dvr_fmt = "div[ng-model='d.cxDriverModel'] input[placeholder='{}']".format
 
-    car_brand = Select(xpath="//div[@id='CarBrandsPanel']/div/select")
-
-    # Параметры автомобиля
-    car_is_new = Label(css="label[for='isCarNew-Y']")
-    car_is_mileage = Label(css="label[for='isCarNew-N']")
-    car_mileage = Input(css="input#carMileage")
-    car_using_start_date = Input(css="input#KaskoCarUsingStartDate")
-    car_night_parking_type_1 = Label(css="label[for='night1']")
-    car_night_parking_type_2 = Label(css="label[for='night2']")
-    is_credit_car_yes = Label(css="label[for='credit-y']")
-    is_credit_car_no = Label(css="label[for='credit-n']")
-    car_autostart_yes = Label(css="label[for='auto-y']")
-    car_autostart_no = Label(css="label[for='auto-n']")
-    car_price = Input(css="input#KaskoAvgCarPrice")
-    antitheft_included = Label(css="label[for='antitheft1']")
-    antitheft_other = Label(css="label[for='antitheft2']")
-    antitheft_system_models = Select(css="select#KaskoAntitheftSystemModels")
-    insure_additional_equipment = Label(
-        css="label[for='KaskoInsureAdditionalEquipment']")
-    audio_equipment_sum = Input(css="input#AudioEquipmentSum")
-    other_equipment_sum = Input(css="input#OtherEquipmentSum")
-
-    # Данные о лицах, допущенных к управлению
-    multi_drive = Label(css="label[for='tp_MultiDrive']")
-    multi_drive_light = Label(css="label[for='tp_MultiDriveLight']")
-    add_usage_driver = Link(id="cmdAddUsageDriverTemplate")
-
-    # Параметры страхового полиса
-    date_begin = Input(css="input#KaskoAgrDateBeg")
-
-    variants = PageElements(css="ul#KaskoVariants li")
-    variant_info = PageElements(css="ul#kaskoCalculationVariantInfo li")
-
-    # Расчет скидки
-    discount_sizepj = Label(css="label[for='TP_DISCOUNTSIZEPJ']")
-
-    calculate = Label(id="calculateButton")
-
-    @property
-    def car_min_price(self):
-        return int(self.webdriver.execute_script(
-            "return $('input#KaskoMinCarPrice').val();"))
-
-    @property
-    def car_max_price(self):
-        return int(self.webdriver.execute_script(
-            "return $('input#KaskoMaxCarPrice').val();"))
-
-    def city(self, value):
-        self.webdriver.execute_script(
-            """
-            var element = $("input#kaskoCityIsn");
-            element.val('%s').keyup().autocomplete("search", element.val());
-            setTimeout(function () {
-                element.autocomplete("widget")
-                    .trigger($.Event("keydown", {
-                        keyCode: $.ui.keyCode.DOWN
-                    }))
-                    .trigger($.Event("keydown", {
-                        keyCode: $.ui.keyCode.ENTER
-                    }));
-            }, 3000);
-            """ % value)
-        sleep(3)
-
-    def _label(self, panel_id, value):
-        return Label(xpath=('//div[@id="%s"]/ul/li/div/'
-                            'label[contains(text(), "%s")]' %
-                            (panel_id, value))).__get__(self, self.__class__)
-
-    def _label_avg(self, panel_id):
-        labels = PageElements(xpath="//div[@id='%s']/ul/li/div/label" %
-                              panel_id).__get__(self, self.__class__)
-        index = int(round((len(labels) / 2.0)))
-        return labels[index]
-
-    @safe
-    def car_model(self, value):
-        self._label("CarModelsPanel", value).click()
-
-    @safe
-    def car_year(self, value):
-        if value:
-            value = int(value)
-
-            labels = PageElements(
-                xpath=('//div[@id="CarManifacturingYearsPanel"]'
-                       '/ul/li/div/label')).__get__(self, self.__class__)
-            years = [int(label.text) for label in labels]
-
-            for i, year in enumerate(years):
-                if value == year:
-                    labels[i].click()
-                    return
-
-            min_year = min(years)
-            max_year = max(years)
-            if value < min_year:
-                labels[years.index(min_year)].click()
-            elif value > max_year:
-                labels[years.index(max_year)].click()
-            else:
-                raise ValueError("Couldn't resolve year: %s" % value)
-
-    @safe
-    def car_engine_model(self):
-        self._label_avg("CarEngineModelsPanel").click()
-
-    @safe
-    def car_modification(self):
-        self._label_avg("CarModificationsPanel").click()
-
-    @safe
-    def car_transmission_type(self, value):
-        self._label("CarTransmissionTypesPanel", value).click()
-
-    def is_credit_car(self, value):
-        if value:
-            value = str(value).strip()
-            if value.lower() == "да":
-                self.is_credit_car_yes.click()
-            elif value.lower() == "нет":
-                self.is_credit_car_no.click()
-            else:
-                raise RuntimeError(
-                    'Invalid value for "is_credit_car": "%s"' % value)
-
-    def car_night_parking_type(self, value):
-        if value:
-            value = str(value).strip()
-            if value == "Охраняемая стоянка / Гараж":
-                self.car_night_parking_type_1.click()
-            elif value == "Иное":
-                self.car_night_parking_type_2.click()
-            else:
-                raise RuntimeError(
-                    'Invalid value for "car_night_parking_type": "%s"' % value)
-
-    def car_autostart(self, value):
-        if value:
-            value = str(value).strip()
-            if value.lower() == "да":
-                self.car_autostart_yes.click()
-            elif value.lower() == "нет":
-                self.car_autostart_no.click()
-            else:
-                raise RuntimeError(
-                    'Invalid value for "car_autostart": "%s"' % value)
-
-    def driver_age(self, index, value):
-        return Input(css="input#KaskoDriverAge%d" % index).__set__(
-            self, value)
-
-    def driver_experience_years(self, index, value):
-        return Input(css="input#KaskoDriverExperienceYears%d" % index).__set__(
-            self, value)
-
-    def driver_sex_male(self, index):
-        return Label(xpath=("//input[@name='KaskoDrivers[%d].Sex' "
-                            "and @value='1']/../label") %
-                     index).__get__(self, self.__class__)
-
-    def driver_sex_female(self, index):
-        return Label(xpath=("//input[@name='KaskoDrivers[%d].Sex' "
-                            "and @value='2']/../label") %
-                     index).__get__(self, self.__class__)
-
-    def driver_has_children_yes(self, index):
-        return Label(xpath=("//input[@name='KaskoDrivers[%d].HasChildren' "
-                            "and @value='1']/../label") %
-                     index).__get__(self, self.__class__)
-
-    def driver_has_children_no(self, index):
-        return Label(xpath=("//input[@name='KaskoDrivers[%d].HasChildren' "
-                            "and @value='2']/../label") %
-                     index).__get__(self, self.__class__)
-
-    def driver_is_married_yes(self, index):
-        return Label(xpath=("//input[@name='KaskoDrivers[%d].IsMarried' "
-                            "and @value='0']/../label") %
-                     index).__get__(self, self.__class__)
-
-    def driver_is_married_no(self, index):
-        return Label(xpath=("//input[@name='KaskoDrivers[%d].IsMarried' "
-                            "and @value='1']/../label") %
-                     index).__get__(self, self.__class__)
-
-    def installment(self, value):
-        if not str(value):
-            return
-
-        locator = "label[for='once%d']" % 1 if value == "Единовременно" else 2
-        try:
-            Label(css=locator).__get__(self, self.__class__)
-            self.webdriver.execute_script('$("%s").click();' % locator)
-        except Exception as e:
-            logging.warning(e)
-
-    def wait_hide_loader(self):
-        WebDriverWait(self.webdriver, WAIT_TIMEOUT).until_not(
-            lambda driver: driver.find_element_by_id(
-                "carAdditionalDataSpinner").is_displayed(),
-            "Loader timeout expired")
-
-    def wait_calculation(self):
-        WebDriverWait(self.webdriver, WAIT_TIMEOUT).until_not(
-            lambda driver: driver.find_element_by_id(
-                "spinnerStep1").is_displayed(),
-            "Calculation timeout expired")
-
-    def wait_calculation_update(self):
-        WebDriverWait(self.webdriver, WAIT_TIMEOUT).until_not(
-            lambda driver: driver.find_element_by_id(
-                "calculationUpdateSpinner").is_displayed(),
-            "Calculation update timeout expired")
-
-    @property
-    def has_franchise(self):
-        return self.webdriver.execute_script(
-            """
-            return (function () {{
-                var o = $('input#kaskoIncludeFranchise1');
-                return o.is(':visible') && o.is(':checked');
-            }})();
-            """)
-
-    @property
-    def franchise_value(self):
-        return self.webdriver.execute_script(
-            """
-            return $("select#KaskoFranchise1Limits option").filter(
-                ':selected').text();
-            """)
-
-    @property
-    def result(self):
-        return self.webdriver.find_element_by_id(
-            "kaskoTopResultProductDescription").text
-
-    @property
-    def errors(self):
-        errors = self.webdriver.find_elements_by_xpath(
-            "//ul[@class='errors-list']/li")
-        return "\n".join([error.text for error in errors])
-
-    def next_variant(self):
-        self.webdriver.execute_script("$('a.flex-next').click();")
+    city = SearchTextbox(css="div[city='vm.regCity.city']")
+    car_brand = MultySelectTextbox(css=car_fmt("Марка автомобиля"))
+    car_year = MultySelectTextbox(css=car_fmt("Год выпуска"))
+    car_model = MultySelectTextbox(css=car_fmt("Модель"))
+    car_transmission_type = MultySelectTextbox(css=car_fmt("Коробка передач"))
+    car_engine_type = MultySelectTextbox(css=car_fmt("Тип двигателя"))
+    car_modification = MultySelectTextbox(css=car_fmt("Тип кузова"))
+    car_engine_model = MultySelectTextbox(css=car_fmt("Двигатель"))
+    car_mileage = MultySelectTextbox(css=car_fmt("Текущий пробег"))
+    car_is_new = MultySelectTextbox(css=car_fmt(
+        "Дата покупки первым собственником"))
+    antitheft_system = MultySelectTextbox(css=car_fmt(
+        "Противоугонный комплекс"))
+    car_autostart = MultySelectTextbox(css=car_fmt("Автозапуск"))
+    is_credit_car = MultySelectTextbox(css=car_fmt("Автомобиль в кредите ?"))
+    add_driver = Link(link_text="+ Добавить водителя")
+    driver_age = MultySelectTextbox(css=dvr_fmt("Возраст водителя"))
+    driver_experience = MultySelectTextbox(css=dvr_fmt("Стаж водителя"))
+    driver_sex = MultySelectTextbox(css=dvr_fmt("Пол водителя"))
+    driver_is_married = MultySelectTextbox(css=dvr_fmt("Состояние в браке"))
+    driver_has_children = MultySelectTextbox(css=dvr_fmt("Дети"))
+    car_start_using_date = Textbox(
+        css="div[ng-model='vm.CarParams.StartUsingDate'] input")
+    car_price = Slider(css="div[ng-model='vm.SumSlider.value'] input")
+    calculate = Button(css="button[ng-model='vm.isSubmitting']")
+    cover_type = Select(id="kaskoCalculationCoverTypeSelect")
+    variants = VariantsSlider(css="div[ng-model='kcc.selectedVariantNumber']")
